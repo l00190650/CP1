c0e9ca6470d011918147a3e2e866dccf2aa1a86e
vo1ter <me@vo1ter.me>
2026-02-23 13:09:30 +0000

    Completed LeapYear practical

--- Week 6/Practical/Leap/LeapYear/Leap.cs
+++ Week 6/Practical/Leap/LeapYear/Leap.cs
@@ -1,24 +1,24 @@
-﻿/* 
+﻿/*
  * Filename: Leap.cs
  * Description: A skeleton class for a C# practical exercise covering selection statements in C#
- * 
+ *
  * Excercise adapted from https://github.com/exercism/csharp/tree/main/exercises/practice/leap
  */
 
 /*
 * Instructions:
-* Implement the method below that checks if a date is a leap year. 
+* Implement the method below that checks if a date is a leap year.
 * A leap year (in the Gregorian calendar) occurs:
 * In every year that is evenly divisible by 4.
-* Unless the year is evenly divisible by 100, in which case it's only a leap year if the year 
+* Unless the year is evenly divisible by 100, in which case it's only a leap year if the year
 * is also evenly divisible by 400.
-* 
+*
 * Some examples:
 *
 *   1997 was not a leap year as it's not divisible by 4.
 *   1900 was not a leap year as it's not divisible by 400.
 *   2000 was a leap year!
-* 
+*
 */
 
 using System;
@@ -36,8 +36,7 @@ namespace LeapYear
         /// <exception cref="NotImplementedException"></exception>
         public static bool IsLeapYear(int year)
         {
-            //TODO Implement the IsLeapYear method
-            throw new NotImplementedException("You need to implement this function.");
+            return DateTime.IsLeapYear(year);
         }
     }
 }
