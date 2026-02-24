2b10267e8b3850271513050194aeb09a8601b4bf
vo1ter <me@vo1ter.me>
2026-02-24 16:49:54 +0000

    Completed Scrabble practical

--- Week 6/Practical/Scrabble/Scrabble/ScrabbleScore.cs
+++ Week 6/Practical/Scrabble/Scrabble/ScrabbleScore.cs
@@ -1,8 +1,8 @@
-﻿/* 
+﻿/*
  * Filename: Lasagna.cs
  * Description: A skeleton class for a C# practical exercise covering basic programming tasks
- * such as declaring variables, methods, returning data from methods. 
- * 
+ * such as declaring variables, methods, returning data from methods.
+ *
  * Excercise adapted from https://github.com/exercism/csharp/tree/main/exercises/practice/scrabble-score
  */
 
@@ -11,7 +11,37 @@ using System.Collections.Generic;
 namespace Scrabble
 {
     public static class ScrabbleScore
-    {        
+    {
+        private static Dictionary<char, int> letterScore = new Dictionary<char, int>()
+        {
+            { 'A', 1 },
+            { 'E', 1 },
+            { 'I', 1 },
+            { 'O', 1 },
+            { 'U', 1 },
+            { 'L', 1 },
+            { 'N', 1 },
+            { 'R', 1 },
+            { 'S', 1 },
+            { 'T', 1 },
+            { 'D', 2 },
+            { 'G', 2 },
+            { 'B', 3 },
+            { 'C', 3 },
+            { 'M', 3 },
+            { 'P', 3 },
+            { 'F', 4 },
+            { 'H', 4 },
+            { 'V', 4 },
+            { 'W', 4 },
+            { 'Y', 4 },
+            { 'K', 5 },
+            { 'J', 8 },
+            { 'X', 8 },
+            { 'Q', 10 },
+            { 'Z', 10 },
+        };
+
         /// <summary>
         /// Calculates the score based on the specified input string.Each letter in the input string is scored
         /// according to the following rules:
@@ -28,10 +58,27 @@ namespace Scrabble
         /// <returns>An integer representing the calculated score for the provided input.</returns>
         public static int Score(string input)
         {
-            // TODO - Implement the Score method. Hint you can implement this without selection statements. Think
-            // about how what data you need and how you could store the date. Use your knowledge of the content
-            // from the algorithms and data structures module you are also studying.
-            throw new NotImplementedException("You need to implement this method.");
+            int score = 0;
+            if (
+                input == null ||
+                input == ""
+            ) return score; //throw new ArgumentNullException(nameof(input), "Input cannot be null or empty.");
+            input = input.ToUpper();
+            char[] inputChars = input.ToCharArray();
+
+            foreach(char chara in inputChars)
+            {
+                try
+                {
+                    score += letterScore[chara];
+                }
+                catch (KeyNotFoundException e)
+                {
+                    Console.WriteLine($"Couldn't find character (chara) in the dictionary; skipping it");
+                }
+            }
+
+            return score;
         }
 
     }
